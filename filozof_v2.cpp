e7e118b761fe4316adccff9b67a4ba8b2a21386f
prezes96 <gmilaszkiewicz@gmail.com>
2018-03-15 23:34:46 +0100

    thread test

--- filozof_v2.cpp
+++ filozof_v2.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
+const int numThreads = 5;
+
+void startThread(int tID) {
+	cout << "Filozof " << tID <<" zasiadl do stolu." <<endl;
+}
+
+
 int main(){
-    cout<<"hello world"<<endl;
-    return 0;
+	srand(time(NULL));
+	thread t[numThreads];
+
+	for (int i = 0; i < numThreads; i++)
+	{
+		t[i] = thread(startThread, i);
+	}
+	for (int i = 0; i < numThreads; i++)
+	{
+		t[i].join();
+	}
+
+	return 0;
 }
